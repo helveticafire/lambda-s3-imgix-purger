6cf08cd850b2d9589dec726834677fcbe0bf6da2
Ben Fitzgerald <theeuclidean@gmail.com>
2016-02-17 23:31:41 +0100

    Single quotes all the way

--- lambda_function.py
+++ lambda_function.py
@@ -10,18 +10,18 @@ print('Loading function')
 def lambda_handler(event, context):
     # print("Received event: " + json.dumps(event, indent=2))
 
-    print("Handler started!")
+    print('Handler started!')
     key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key']).decode('utf8')
-    purge_endpoint = "https://api.imgix.com/v2/image/purger"
+    purge_endpoint = 'https://api.imgix.com/v2/image/purger'
     results = {}
     with open('config.json') as config_file:
         config = json.load(config_file)
         domains = config['domains']
         for domain, v in domains.iteritems():
             for scheme in v['schemes']:
-                to_purge = scheme + '://'+ domain + '/' + key
-                print("Going to purge: " + to_purge)
-                req = requests.post(purge_endpoint, auth=(config['api_key'], ''), data={"url": to_purge})
-                print(to_purge + " response status code: " + str(req.status_code))
+                to_purge = scheme + '://' + domain + '/' + key
+                print('Going to purge: ' + to_purge)
+                req = requests.post(purge_endpoint, auth=(config['api_key'], ''), data={'url': to_purge})
+                print(to_purge + ' response status code: ' + str(req.status_code))
                 results[to_purge] = req.status_code
     return results
