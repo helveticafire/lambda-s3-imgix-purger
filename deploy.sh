9e402f47ff27dbb13c8a1e9332c07a4ed60ab2d7
Ben Fitzgerald <theeuclidean@gmail.com>
2016-02-17 01:02:57 +0100

    Deployment file improvements

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-# Verify that there is a config file
-# Make sure it is valid json
-# Parse requirements file
-# make a zip of all to temp director
-dst="$TMPDIR/lambda.zip"
-zip -r dst lambda_function.py config.json env/lib/python2.7/site-packages/requests/ env/lib/python2.7/site-packages/requests-2.9.1.dist-info/
-aws lambda update-function-code --function-name $LAMBDA_FN_NAME  --zip-file fileb://$TMPDIR/lambda.zip
+set -euo pipefail
+set -x
+
+# TODO Verify that there is a config file
+# TODO Make sure it is valid json
+# TODO Parse requirements file
+
+dst_zip="lambda-$(date +%s).zip"
+dst="$TMPDIR$dst_zip"
+
+zip -r "$dst" lambda_function.py config.json env/lib/python2.7/site-packages/requests/ env/lib/python2.7/site-packages/requests-2.9.1.dist-info/
+
+aws lambda update-function-code --function-name "$LAMBDA_FN_NAME"  --zip-file fileb://"$dst"
