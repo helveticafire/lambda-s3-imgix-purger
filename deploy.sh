fb22048c51e65b36ac10e5c00d544ba1d4b5deb5
Ben Fitzgerald <theeuclidean@gmail.com>
2016-02-29 00:23:51 +0100

    Clean up of deploy.sh
    Using bash functions
    Better handling of creating temporary resources

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,44 @@
 set -euo pipefail
 set -x
 
-# TODO Verify that there is a config file
-# https://github.com/helveticafire/lambda-s3-imgix-purger/issues/7
-# TODO Ensure config file is valid json
-# https://github.com/helveticafire/lambda-s3-imgix-purger/issues/8
-# TODO Make a distribution requirements file for production use
-# https://github.com/helveticafire/lambda-s3-imgix-purger/issues/9
-
 dst_zip="lambda-$(date +%s).zip"
 dst="$TMPDIR$dst_zip"
+temp="temp-$(date +%s)"
+temp_dir="$TMPDIR$temp"
 
-cp -r env/lib/python2.7/site-packages/requests/ requests/
-cp -r env/lib/python2.7/site-packages/requests-2.9.1.dist-info/ requests-2.9.1.dist-info/
+create_zip() {
+    mkdir "$temp_dir"
 
-zip -r "$dst" lambda_function.py config.json requests requests-2.9.1.dist-info
+    # TODO Make a distribution requirements file for production use
+    # https://github.com/helveticafire/lambda-s3-imgix-purger/issues/9
 
-rm -rf requests/
-rm -rf requests-2.9.1.dist-info/
+    cp -r env/lib/python2.7/site-packages/requests/ "$temp_dir/requests/"
+    cp -r env/lib/python2.7/site-packages/requests-2.9.1.dist-info/ "$temp_dir/requests-2.9.1.dist-info/"
 
-aws lambda update-function-code --function-name "$LAMBDA_FN_NAME"  --zip-file fileb://"$dst" --publish
+    # TODO Verify that there is a config file
+    # https://github.com/helveticafire/lambda-s3-imgix-purger/issues/7
+    # TODO Ensure config file is valid json
+    # https://github.com/helveticafire/lambda-s3-imgix-purger/issues/8
+
+    cp {lambda_function.py,config.json} "$temp_dir/."
+
+    pushd "$temp_dir"
+
+    zip -r "$dst" *
+
+    popd
+    echo "Created uploadable zip at: $dst"
+}
+
+check_fn_name() {
+    echo "$LAMBDA_FN_NAME" > /dev/null
+}
+
+update() {
+    check_fn_name
+    create_zip
+    aws lambda update-function-code --function-name "$LAMBDA_FN_NAME" --zip-file fileb://"$dst" --publish
+}
+
+# call arguments verbatim:
+"$@"
