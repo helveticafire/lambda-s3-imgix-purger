d61bc402b7e15e2db8e5361c964a4c041be57804
Ben Fitzgerald <theeuclidean@gmail.com>
2016-02-29 03:03:43 +0100

    Validating runtime configuration

--- lambda_function.py
+++ lambda_function.py
@@ -38,13 +38,72 @@ def lambda_handler(event, context):
     purge_endpoint = 'https://api.imgix.com/v2/image/purger'
     results = {}
     with open(config_file_name) as config_file:
-        config = json.load(config_file)
-        domains = config['domains']
-        for domain, v in domains.iteritems():
-            for scheme in v['schemes']:
-                to_purge = scheme + '://' + domain + '/' + key
+        try:
+            config = json.load(config_file)
+        except (ValueError, AttributeError, TypeError) as err:
+            print('Config was not valid, error: {}, {}'.format(err, config_file.read()))
+            return {}
+
+        try:
+            domains = config['domains']
+            api_key = config['api_key']
+        except (KeyError, TypeError) as err:
+            print('Config was not valid, error: {}, {}'.format(err, config))
+            return {}
+
+        type_domains = type(domains)
+        if type_domains is not dict:
+            print('Domains was not a dict, it was {}'.format(type_domains))
+            return {}
+        if len(domains) < 1:
+            print('Domains has no items')
+            return {}
+
+        type_api_key = type(api_key)
+        if type_api_key is not unicode:
+            print('api_key was not a unicode, it was {}'.format(type_api_key))
+            return {}
+        if len(api_key) <= 1:
+            print('api_key needs to have a value')
+            return {}
+
+        allow_schemes = ['http', 'https']
+        for k, v in domains.iteritems():
+            if len(k) < 1:
+                print('Config was not valid - schemes has no items')
+                return {}
+            if '/' in k:
+                print('Config was not valid - Domains cannot contain forward lash')
+                return {}
+            if not k.endswith('.imgix.net'):
+                # see https://docs.imgix.com/setup/serving-images
+                print('Config was not valid - Domains must end with .imgix.net was {}'.format(k))
+                return {}
+
+            type_v = type(v)
+            if type_v is not dict:
+                print('{} must be a dict value, it was {}'.format(v, type_v))
+                return {}
+
+            try:
+                schemes = v['schemes']
+            except (KeyError, TypeError) as err:
+                print('Config was not valid - schemes, error: {}, {}'.format(err, config))
+                return {}
+
+            type_schemes = type(schemes)
+            if type_schemes is not list:
+                print('{} must be a list, it was {}'.format(v, type_schemes))
+                return {}
+
+            for scheme in schemes:
+                if scheme not in allow_schemes:
+                    print('Config was not valid - schemes only allow http; https: value was {}'.format(scheme))
+                    return {}
+                to_purge = scheme + '://' + k + '/' + key
                 print('Going to purge: ' + to_purge)
-                req = requests.post(purge_endpoint, auth=(config['api_key'], ''), data={'url': to_purge})
-                print(to_purge + ' response status code: ' + str(req.status_code))
-                results[to_purge] = req.status_code
+                resp = requests.post(purge_endpoint, auth=(api_key, ''), data={'url': to_purge})
+                print(to_purge + ' response status code: ' + str(resp.status_code))
+                results[to_purge] = resp.status_code
+    print('Handler finished!')
     return results
--- lambda_hander_test.py
+++ lambda_hander_test.py
@@ -1,8 +1,20 @@
 import unittest
 from lambda_function import lambda_handler
+from mock import mock_open, patch, Mock
+import __builtin__ as builtins
+import json
 
 
 class LambdaHandler(unittest.TestCase):
+
+    def setUp(self):
+        self.basic_types = ['', u'', 'a', 'hello-str',
+                            u'hello-unicode',
+                            1, -1, 0, 1L, 0.0, True, False]
+        self.basic_collections = [[], {}, (), [1, 2, 3], {'': ''}, {'1': 'a'}, (1, 2, 3)]
+        self.json_exclude = [1.0j]
+        self.the_basics = self.basic_types + self.basic_collections
+        self.valid_notification_event = {'Records': [{'s3': {'object': {'key': 'blah'}}}]}
 
     def test_ensure_event_is_dict(self):
         self.assertEqual(lambda_handler('', ''), {})
@@ -33,16 +45,153 @@ class LambdaHandler(unittest.TestCase):
         self.assertEqual(lambda_handler(notification_event, ''), {})
         notification_event = {'Records': [{'s3': {'object': {'key': ''}}}]}
         self.assertEqual(lambda_handler(notification_event, ''), {})
-        notification_event = {'Records': [{'s3': {'object': {'key': 'blah'}}}]}
-        # self.assertEqual(lambda_handler(notification_event, ''), {})
+        self.assertEqual(lambda_handler(self.valid_notification_event, ''), {})
 
     def test_s3_key_for_dir(self):
         notification_event = {'Records': [{'s3': {'object': {'key': 'blah/'}}}]}
         self.assertEqual(lambda_handler(notification_event, ''), {})
 
     def test_no_config(self):
-        notification_event = {'Records': [{'s3': {'object': {'key': 'blah'}}}]}
-        self.assertEqual(lambda_handler(notification_event, ''), {})
+        self.assertEqual(lambda_handler(self.valid_notification_event, ''), {})
+
+    def test_json_config_type_handling(self):
+        with patch('os.path.isfile') as isfile_mock:
+            instance = isfile_mock.return_value
+            instance.method.return_value = True
+
+            for x in self.the_basics:
+                mock_data = json.dumps(x)
+                with patch.object(builtins, 'open', mock_open(read_data=mock_data)):
+                    self.assertEqual(lambda_handler(self.valid_notification_event, ''), {})
+
+    def test_domain_type_handling_config(self):
+        with patch('os.path.isfile') as isfile_mock:
+            instance = isfile_mock.return_value
+            instance.method.return_value = True
+
+            for x in self.the_basics:
+                mock_data = json.dumps({'domains': x})
+                with patch.object(builtins, 'open', mock_open(read_data=mock_data)):
+                    self.assertEqual(lambda_handler(self.valid_notification_event, ''), {})
+
+    def test_api_key_type_handling_config(self):
+        with patch('os.path.isfile') as isfile_mock:
+            instance = isfile_mock.return_value
+            instance.method.return_value = True
+
+            for x in self.the_basics:
+                mock_data = json.dumps({'api_key': x})
+                with patch.object(builtins, 'open', mock_open(read_data=mock_data)):
+                    self.assertEqual(lambda_handler(self.valid_notification_event, ''), {})
+
+
+    def test_config_plain_text_handling(self):
+        with patch('os.path.isfile') as isfile_mock:
+            instance = isfile_mock.return_value
+            instance.method.return_value = True
+
+            for x in self.the_basics + self.json_exclude:
+                with patch.object(builtins, 'open', mock_open(read_data=x)):
+                    self.assertEqual(lambda_handler(self.valid_notification_event, ''), {})
+
+    def test_domains_dict_type_handling_config(self):
+        with patch('os.path.isfile') as isfile_mock:
+            instance = isfile_mock.return_value
+            instance.method.return_value = True
+
+            for x in self.basic_types:
+                for y in reversed(self.the_basics):
+                    mock_data = json.dumps({'api_key': 'example',
+                                            'domains': {x: y}})
+                    with patch.object(builtins, 'open', mock_open(read_data=mock_data)):
+                        self.assertEqual(lambda_handler(self.valid_notification_event, ''), {})
+
+    def test_imgix_domain_forward_lash_check_source_config(self):
+        with patch('os.path.isfile') as isfile_mock:
+            instance = isfile_mock.return_value
+            instance.method.return_value = True
+
+            mock_data = json.dumps({'api_key': 'example',
+                                    'domains': {'/sdafasdf/': ''}})
+            with patch.object(builtins, 'open', mock_open(read_data=mock_data)):
+                self.assertEqual(lambda_handler(self.valid_notification_event, ''), {})
+
+    def test_imgix_correct_domain_source_config(self):
+        with patch('os.path.isfile') as isfile_mock:
+            instance = isfile_mock.return_value
+            instance.method.return_value = True
+
+            mock_data = json.dumps({'api_key': 'example',
+                                    'domains': {'dsaf.not.imgix.net': ''}})
+            with patch.object(builtins, 'open', mock_open(read_data=mock_data)):
+                self.assertEqual(lambda_handler(self.valid_notification_event, ''), {})
+
+    def test_domain_and_api_key_types_handling_config(self):
+        with patch('os.path.isfile') as isfile_mock:
+            instance = isfile_mock.return_value
+            instance.method.return_value = True
+
+            for x in self.the_basics:
+                for y in reversed(self.the_basics):
+                    mock_data = json.dumps({'api_key': x,
+                                            'domains': y})
+                    with patch.object(builtins, 'open', mock_open(read_data=mock_data)):
+                        self.assertEqual(lambda_handler(self.valid_notification_event, ''), {})
+
+    def test_domain_url_dict_types_handling_config(self):
+        with patch('os.path.isfile') as isfile_mock:
+            instance = isfile_mock.return_value
+            instance.method.return_value = True
+
+            for x in self.basic_types:
+                for y in reversed(self.the_basics):
+                    mock_data = json.dumps({'api_key': 'example',
+                                            'domains': {'dsaf.not.imgix.net': {x: y}}})
+                    with patch.object(builtins, 'open', mock_open(read_data=mock_data)):
+                        self.assertEqual(lambda_handler(self.valid_notification_event, ''), {})
+
+    def test_scheme_types_handling_config(self):
+        with patch('os.path.isfile') as isfile_mock:
+            instance = isfile_mock.return_value
+            instance.method.return_value = True
+
+            for y in reversed(self.the_basics):
+                mock_data = json.dumps({'api_key': 'example',
+                                        'domains': {'dsaf.not.imgix.net': {'schemes': y}}})
+                with patch.object(builtins, 'open', mock_open(read_data=mock_data)):
+                    self.assertEqual(lambda_handler(self.valid_notification_event, ''), {})
+
+    def test_correct_config(self):
+        with patch('os.path.isfile') as isfile_mock:
+            instance = isfile_mock.return_value
+            instance.method.return_value = True
+
+            mock_data = json.dumps({'api_key': 'example',
+                                    'domains': {'dsaf.not.imgix.net': {'schemes': ['http', 'https']}}})
+            with patch.object(builtins, 'open', mock_open(read_data=mock_data)):
+                with patch('requests.post') as req_post:
+                    req_post.return_value = mock_response = Mock()
+                    mock_response.status_code = 200
+                    self.assertEqual(lambda_handler(self.valid_notification_event, ''),
+                                     {'http://dsaf.not.imgix.net/blah': 200,
+                                      'https://dsaf.not.imgix.net/blah': 200})
+
+    def test_correct_config_multi_domains(self):
+        with patch('os.path.isfile') as isfile_mock:
+            instance = isfile_mock.return_value
+            instance.method.return_value = True
+
+            mock_data = json.dumps({'api_key': 'example',
+                                    'domains': {'dsaf.not.imgix.net': {'schemes': ['http', 'https']},
+                                                '1234.imgix.net': {'schemes': ['https']}}})
+            with patch.object(builtins, 'open', mock_open(read_data=mock_data)):
+                with patch('requests.post') as req_post:
+                    req_post.return_value = mock_response = Mock()
+                    mock_response.status_code = 200
+                    self.assertEqual(lambda_handler(self.valid_notification_event, ''),
+                                     {'http://dsaf.not.imgix.net/blah': 200,
+                                      'https://dsaf.not.imgix.net/blah': 200,
+                                      'https://1234.imgix.net/blah': 200})
 
 
 if __name__ == '__main__':
